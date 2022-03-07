9ce5208b785dc1b2713a4fb56a82d59c6df32935
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2022-03-07 23:57:39 +0700

    first push

---
+++ Project_PRN211_Group3/GUI/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PRN211_Group3.GUI
+{
+    public class Settings
+    {
+        public static string UserName { get; set; } = "";
+        public static int UserID { get; set; } = -1;
+    }
+}
