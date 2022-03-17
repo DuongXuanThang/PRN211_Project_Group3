a41b4dfdb3ff153b6948c69eebcaa8862a264709
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2022-03-17 13:13:05 +0700

    update

--- WebQuanLyShopBanHang/WebQuanLyShopBanHang/Models/Group3Context.cs
+++ WebQuanLyShopBanHang/WebQuanLyShopBanHang/Models/Group3Context.cs
@@ -18,6 +18,8 @@ namespace WebQuanLyShopBanHang.Models
         }
 
         public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -57,6 +59,63 @@ namespace WebQuanLyShopBanHang.Models
                     .WithMany(p => p.Accounts)
                     .HasForeignKey(d => d.RoleId)
                     .HasConstraintName("FK_Accounts_Roles");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(e => e.CatId);
+
+                entity.Property(e => e.CatId).HasColumnName("CatID");
+
+                entity.Property(e => e.Alias).HasMaxLength(250);
+
+                entity.Property(e => e.CatName).HasMaxLength(250);
+
+                entity.Property(e => e.Cover).HasMaxLength(255);
+
+                entity.Property(e => e.MetaDesc).HasMaxLength(250);
+
+                entity.Property(e => e.MetaKey).HasMaxLength(250);
+
+                entity.Property(e => e.ParentId).HasColumnName("ParentID");
+
+                entity.Property(e => e.Thumb).HasMaxLength(250);
+
+                entity.Property(e => e.Title).HasMaxLength(250);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.Alias).HasMaxLength(255);
+
+                entity.Property(e => e.CatId).HasColumnName("CatID");
+
+                entity.Property(e => e.DateCreated).HasColumnType("datetime");
+
+                entity.Property(e => e.DateModified).HasColumnType("datetime");
+
+                entity.Property(e => e.MetaDesc).HasMaxLength(255);
+
+                entity.Property(e => e.MetaKey).HasMaxLength(255);
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.ShortDesc).HasMaxLength(255);
+
+                entity.Property(e => e.Thumb).HasMaxLength(255);
+
+                entity.Property(e => e.Title).HasMaxLength(255);
+
+                entity.Property(e => e.Video).HasMaxLength(255);
+
+                entity.HasOne(d => d.Cat)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CatId)
+                    .HasConstraintName("FK_Products_Categories");
             });
 
             modelBuilder.Entity<Role>(entity =>
